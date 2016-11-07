afc2f751f6a5f348c84f4b82c23cf8a3d128cf2d
Nathan Hoad <nathan@nathanhoad.net>
2016-11-07 16:37:17 +1000

    Added more video formats

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const KnexCleaner = require('knex-cleaner');
 // Mock out `require()` to fake CSS and images
 var originalLoader = Module._load;
 function mockedLoader (path, parent, is_main) {
-    if (path.match(/\.(png|jpg|jpeg|gif|mp4|m4v|flv|mp3|wav|m4a)$/)) {
+    if (path.match(/\.(png|jpg|jpeg|gif|mp4|m4v|flv|mp3|wav|m4a|ogg|ogv|ogm|webm)$/)) {
         return {};
     }
     
